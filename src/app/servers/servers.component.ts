6f1fb2cfad2796be8061125a115a3fe8c39e2eb2
anila-aribandi <anilachoudary99@gmail.com>
2023-05-07 20:26:08 +0530

    first commit

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -2,11 +2,29 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-servers',
-  template: `
-   <app-server></app-server>
-   <app-server></app-server>`,
+ // template: `
+ // <app-server></app-server>
+// <app-server></app-server>`,
+templateUrl: './servers.component.html',
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent {
+  allowNewServer = false;
+  serverCreationStatus = 'No server was created!';
+  serverName = 'Testserver';
+
+  constructor() {
+    setTimeout(() => {
+      this.allowNewServer = true;
+    }, 2000);
+  }
+  ngOnInit() {
+  }
+  onCreateServer() {
+    this.serverCreationStatus = 'server was created! Name is ' + this.serverName;
+  }
+  onUpdateServerName(event: Event) {
+    this.serverName = (<HTMLInputElement>event.target).value;
+  }
 
 }
